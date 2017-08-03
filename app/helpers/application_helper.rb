ddf9ee1ed0cbe68062b8e2aecb2d48ecb5ec009f
Anton Styagun <astyagun@gmail.com>
2017-08-03 15:16:10 +0300

    Rename helper for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   BOOTSTRAP_FLASH_CLASSES = {notice: 'alert-success', alert: 'alert-danger'}
 
-  def bootstrap_class_for(flash_type)
-    BOOTSTRAP_FLASH_CLASSES.fetch flash_type.to_sym, flash_type.to_s
+  def bootstrap_class_for_flash(type)
+    BOOTSTRAP_FLASH_CLASSES.fetch type.to_sym, type.to_s
   end
 end

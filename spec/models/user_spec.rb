b59465319ed1addafc95e2e1364e670dfa2047cf
Anton Styagun <astyagun@ya.ru>
2020-05-26 17:23:32 +0300

    Add tests for User#admin? method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,59 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validity' do
-    subject { user.valid? }
+  let(:instance) { build :user }
 
-    let(:user) { build :user }
+  describe 'validity' do
+    subject { instance.valid? }
 
     it { is_expected.to be true }
 
     %i[role email full_name birth_date small_biography].each do |field|
       context "when ##{field} is nil" do
-        before { user[field] = nil }
+        before { instance[field] = nil }
 
         it { is_expected.to be false }
       end
 
       context "when ##{field} is and empty string" do
-        before { user[field] = '' }
+        before { instance[field] = '' }
 
         it { is_expected.to be false }
       end
     end
 
     context 'when #role is manager' do
-      before { user.role = 'manager' }
+      before { instance.role = 'manager' }
 
       it { is_expected.to be false }
     end
 
     context 'when #email is 123' do
-      before { user.email = 123 }
+      before { instance.email = 123 }
 
       it { is_expected.to be false }
     end
 
     context 'when #email is email' do
-      before { user.email = 'email' }
+      before { instance.email = 'email' }
 
       it { is_expected.to be false }
     end
 
     context 'when #email is email@domain' do
-      before { user.email = 'email@domain' }
+      before { instance.email = 'email@domain' }
 
       it { is_expected.to be false }
     end
 
     context 'when user with the same email already exists' do
-      before { create :user, email: user.email }
+      before { create :user, email: instance.email }
 
       it { is_expected.to be false }
     end
 
     context 'when password is changed' do
-      before { user.assign_attributes password: password, password_confirmation: password_confirmation }
+      before { instance.assign_attributes password: password, password_confirmation: password_confirmation }
 
       let(:password) { '12345678' }
       let(:password_confirmation) { password }
@@ -86,9 +86,9 @@ RSpec.describe User, type: :model do
     end
 
     context 'when password is not changed (but some other field is changed)' do
-      before { user.small_biography = '123' }
+      before { instance.small_biography = '123' }
 
-      let(:user) { described_class.find create(:user).id }
+      let(:instance) { described_class.find create(:user).id }
 
       it { is_expected.to be true }
     end
@@ -103,6 +103,18 @@ RSpec.describe User, type: :model do
       subject { build :user, :admin }
 
       it { is_expected.to be_valid }
+    end
+  end
+
+  describe '#admin?' do
+    subject { instance.admin? }
+
+    it { is_expected.to be false }
+
+    context 'when user is an admin' do
+      before { instance.role = 'admin' }
+
+      it { is_expected.to be true }
     end
   end
 end
